d642f67d9090f0cb814986bd1d735b142485ac38
Charlotte Lokko <littlelokko@hotmail.com>
2022-11-02 17:49:54 +0000

    medium challenges done

--- MediumChallenges/app.js
+++ MediumChallenges/app.js
@@ -130,10 +130,155 @@ console.log("Q7: " +  getMax([-300, -100, -200]));
 
 function reverseString(str){
     let reversed = "";
-    for(let i= str.length-1; i >= 0; i--){
-        reversed += str.charAt(i);
+    for(let i= str.length; i >=  0; i--){
+        reversed += str.charAt(i); //can use str[i] instead of charAt
     }
     return reversed;
 }
 
 console.log("Q8: " + reverseString("This is cool"));
+
+// Other ways to do reverseString
+
+//incrementing for loop
+function reverseString2(str){
+    let reversed = "";
+    for(let i=0; i < str.length; i++){
+        //This is how you loop through elements in a string
+        reversed = str[i] + reversed;
+    }
+    return reversed;
+}
+
+console.log("Q8i: " + reverseString2("This is cool"));
+
+//array reverse method
+function reverseString3(str){
+    return str.split("").reverse().join("");
+}
+
+console.log("Q8ii: " + reverseString3("This is cool"));
+
+
+/**
+ * Q9: Turn every element in an array into 0
+ * Given an array of elements, return the same length array filled with 0's
+ * 
+ * eg. convertToZeros([5, 100, 4]) => [0,0,0]
+ */
+
+function convertToZeros(arr){
+    let newArr = []
+    for(let i=0; i< arr.length; i++){
+        newArr[i] = 0;
+    }
+    return newArr;
+}
+
+console.log("Q9: " + convertToZeros([1, 2, 3, 4, 5, 6]));
+
+//Other methods
+function convertToZeros2(arr){
+    return new Array(arr.length).fill(0); //create new array and fill with 0's
+}
+
+console.log("Q9i: " + convertToZeros2([1, 2, 3, 4, 5, 6]));
+
+function convertToZeros3(arr){
+    return arr.map(e => 0)
+}
+
+console.log("Q9ii: " + convertToZeros3([1, 2, 3, 4, 5, 6]));
+
+/**
+ * Q10. Filter out all the apples
+ * Given an array of fruits, if it is an apple removie it from the array.
+ * 
+ * eg. removeApples(["Banana", "Apple", "Orange", "Apple"]) => ["Banana", "Orange"]
+ */
+
+function removeApples(arr){
+    let noApples = [];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== "Apple"){
+            noApples.push(arr[i]);
+        }
+    }
+    return noApples;
+}
+
+console.log("Q10: " + removeApples(["Banana", "Apple", "Orange", "Apple"])); 
+
+//Using .filter method
+function removeApples2(arr){
+    return arr.filter(e => e !== "Apple");
+}
+
+console.log("Q10i: " + removeApples2(["Banana", "Apple", "Orange", "Apple"]));
+
+/**
+ * Q11: Filter out all the falsy values
+ * Given an array of values, filter out all the falsy values and only return the truthy values
+ * 
+ * eg. filterOutFalsy(["", [], 0, null, undefined, "0"]) => [[], "0"]
+ */
+
+function filterOutFalsy(arr){
+    let noFalsy = [];
+    for(let i=0; i<arr.length; i++){
+        if(!!arr[i]){
+            noFalsy.push(arr[i]);
+        }
+    }
+    return noFalsy;
+}
+
+console.log("Q11: " + filterOutFalsy(["", [], 0, null, undefined, "0"]));
+ 
+function filterOutFalsy2(arr){
+    return arr.filter(e => !!e === true);
+}
+
+console.log("Q11i: " + filterOutFalsy2(["", [], 0, null, undefined, "0"]));
+
+/**
+ * Q12: Truthy to true, Falsy to false
+ * Given an array of truthy and falsy values, return the same array of elements into its boolean value
+ * 
+ * eg. convertToBoolean([500, 0, "David", "", []]) => [true, false, true, false, true]
+ */
+
+function convertToBoolean(arr){
+    let arrBool = [];
+    for(let i=0; i<arr.length; i++){
+        if(!!arr[i]){
+            arrBool.push(true);
+        }
+        else{
+            arrBool.push(false);
+        }
+    }
+    return arrBool;
+}
+
+console.log("Q12: " + convertToBoolean([500, 0, "David", "", []]));
+
+
+//simplifing with turnary 
+function convertToBoolean2(arr){
+    let arrBool = [];
+    for(let i=0; i<arr.length; i++){
+        !!arr[i] ? arrBool.push(true) : arrBool.push(false);
+    }
+    return arrBool;
+}
+
+console.log("Q12ii: " + convertToBoolean2([500, 0, "David", "", []]));
+
+
+//using map method 
+function convertToBoolean3(arr){
+    return arr.map(e => !!e)
+}
+
+console.log("Q12iii: " + convertToBoolean2([500, 0, "David", "", []]));
